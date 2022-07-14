9db7b11d9d23189e2a9c9f1b7c582170b4cc4827
ferlanuz <ferlaranuz@gmail.com>
2022-07-14 12:07:15 -0500

    función obtener data js, cambio de propiedad css, cambio de etiqueta btn html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+//variable con empty array
+let number = [];
+
+let btn = document.getElementById("btn").addEventListener("click", cardValidation);
+
+function cardValidation (){
+    let cardNumber = document.getElementById("cardNumber").value;
+    for (var i = 0; i < cardNumber.length; i++) {
+        number.push(+cardNumber.charAt(i));
+
+    }
+
+    console.log("este es el array original" + number);
+    
+}
