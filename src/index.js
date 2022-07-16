1042545df2e34062cf02456035c1f790c08add94
ferlanuz <ferlaranuz@gmail.com>
2022-07-16 08:20:04 -0500

    reversa del array, cambios en html y css

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ console.log(validator);
 
 //variable con empty array
 let number = [];
-
+//variable que agrega el evento onclick al botón
 let btn = document.getElementById("btn").addEventListener("click", cardValidation);
-
+//funcion para comenzar a validar tarjeta
 function cardValidation (){
+    //Variable que obtiene el valor del input donde el usuario ingreso el dato
     let cardNumber = document.getElementById("cardNumber").value;
+    //for loop que va utilizar el valor obtenido del input para pushearlo al  empty array
     for (var i = 0; i < cardNumber.length; i++) {
         number.push(+cardNumber.charAt(i));
+        number.reverse(+cardNumber.charAt(i));
 
     }
 
-    console.log("este es el array original" + number);
+    console.log("este es el array original" + number);//expected, "este es el array original 12344567889"
+    console.log("este es el array reverse" + number);
     
 }
