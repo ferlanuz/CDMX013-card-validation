43327e5e2c94a3a435ae2f94fd2076513f80aa31
ferlanuz <ferlaranuz@gmail.com>
2022-07-18 19:22:15 -0500

    import export validator en js, cambio de logo en css, redacción semi final de README

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@ import validator from './validator.js';
 
 console.log(validator);
 
-//variable con empty array
-let number = [];
-//variable que agrega el evento onclick al botón
-let btn = document.getElementById("btn").addEventListener("click", cardValidation);
-//funcion para comenzar a validar tarjeta
-function cardValidation (){
-    //Variable que obtiene el valor del input donde el usuario ingreso el dato
-    let cardNumber = document.getElementById("cardNumber").value;
-    //for loop que va utilizar el valor obtenido del input para pushearlo al  empty array
-    for (var i = 0; i < cardNumber.length; i++) {
-        number.push(+cardNumber.charAt(i));
-        number.reverse(+cardNumber.charAt(i));
 
-    }
+    //dar evento al input
+    document.getElementById("cardNumber").addEventListener("keyup", (e) =>{
+        let inputValue = e.target.value; //Obtener el valor del evento del input y almacenarlo en una variable
+    document.getElementById("cardNumber").value = inputValue. //el input es igual a la variable que contiene el valor
+    replace(/\s/g, "").replace(/\D/g, ""); //quitamos espacios y letras del input .replace()
 
-    console.log("este es el array original" + number);//expected, "este es el array original 12344567889"
-    console.log("este es el array reverse" + number);
-    
-}
+    })
+
+    //dar evento al botón validar
+   document.getElementById("btn").addEventListener("click", validator.cardValidation);
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,68 @@
 const validator = {
-  // ...
-};
+
+//función para comenzar a validar el cc number
+  cardValidation: function cardValidation(){
+
+  let number = []; 
+  let pares = [];
+  let impares = [];
+  let sumTotal = 0;
+  let cont = 1;
+  let valor = document.getElementById("cardNumber").value; //0btener data cc number
+  
+  for (var i = 0; i < valor.length; i++) {
+    number.push(+valor.charAt(i));
+  }
+  number.reverse();
+
+  for (let i = 0; i < number.length; i++) {
+    //impares
+    if (cont % 2 !== 0) {
+      impares.push(number[i]);
+    } else {
+      pares.push(number[i] * 2);
+    }
+    cont++;
+  }
+  console.log("este es el array de impares " + impares);
+  console.log("este es el array de pares " + pares);
+  console.log(typeof pares[3]);
+
+  for (let i = 0; i < pares.length; i++) {
+    if (pares[i] >= 10) {
+      let parToString = `${pares[i]}`;
+      let firstChar = parToString.charAt(0);
+      let secondChar = parToString.charAt(1);
+      console.log(secondChar);
+      let sumPar = parseInt(firstChar) + parseInt(secondChar);
+      console.log(typeof sumPar);
+      pares[i] = sumPar;
+    } else {
+      console.log("nada");
+    }
+  }
+  console.log(pares);
+
+  let arrFinal = pares.concat(impares);
+  console.log(arrFinal);
+
+  for (let i = 0; i < arrFinal.length; i++) {
+    //se suma todo y se calcula
+    sumTotal += arrFinal[i] % 10;
+  }
+  console.log(sumTotal);
+  sumTotal = sumTotal % 10;
+  console.log(sumTotal);
+  if (sumTotal === 0) {
+    return alert("Your credit card is valid. ");
+  } else {
+    return alert("Your credit card is invalid. Please try again. ");
+    
+  }
+
+  
+
+}
+}
 
 export default validator;
